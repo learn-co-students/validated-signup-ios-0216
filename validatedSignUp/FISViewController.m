aa62f0f531123556fec02c3bca051fc4cce82322
susanlovaglio <susan@flatironschool.com>
2017-05-21 00:37:25 -0400

    submit button enable

--- validatedSignUp/FISViewController.m
+++ validatedSignUp/FISViewController.m
@@ -23,13 +23,148 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.firstName.delegate = self;
+    self.lastName.delegate = self;
+    self.email.delegate = self;
+    self.userName.delegate = self;
+    self.password.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self displayConfirmationAlertFor:textField];
+    
+    if ([self validateText: textField]) {
+        
+        if (textField == self.password) {
+            [self.view endEditing:YES];
+            self.submitButton.enabled = YES;
+        }
+        
+        UITextField *nextTF = [self nextTextFieldToEnable:textField];
+        nextTF.enabled = YES;
+        [nextTF becomeFirstResponder];
+        return YES;
+    }
+    
+    return NO;
+}
+
+-(BOOL) validateText:(UITextField *)textField
+{
+    
+    BOOL isValidText = NO;
+    
+    if (textField == self.firstName) {
+        
+        isValidText = [self isValidName:textField.text];
+        
+    } else if (textField == self.lastName) {
+        
+        isValidText = [self isValidName:textField.text];
+        
+    } else if (textField == self.email) {
+        
+        isValidText = [self isValidEmail:textField.text];
+        
+    } else if (textField == self.userName){
+        
+        isValidText = [self isValidName:textField.text];
+        
+    } else if (textField == self.password) {
+        
+        isValidText = [self isValidPassword:textField.text];
+    }
+    
+    return isValidText;
+}
+
+-(BOOL) isValidName: (NSString *) name
+{
+    return name.length > 0 && [name rangeOfCharacterFromSet:NSCharacterSet.decimalDigitCharacterSet].location == NSNotFound;
+}
+
+-(BOOL) isValidEmail: (NSString *) email
+{
+    return email.length > 0 && [email rangeOfString:@"@"].location != NSNotFound && [email rangeOfString:@"."].location != NSNotFound;
+}
+
+-(BOOL) isValidPassword: (NSString *) password
+{
+    return password.length > 6;
+}
+
+-(UITextField *) nextTextFieldToEnable : (UITextField *) currentTextField
+{
+    if (currentTextField == self.firstName) {
+        
+        return self.lastName;
+        
+    } else if (currentTextField == self.lastName) {
+        
+        return self.email;
+        
+    } else if (currentTextField == self.email) {
+        
+        return self.userName;
+        
+    } else if (currentTextField == self.userName) {
+        
+        return self.password;
+    }
+    
+    return nil;
+}
+
+-(void) displayConfirmationAlertFor: (UITextField *)textField
+{
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"Are you Sure"
+                                          message:@"select"
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *cancelAction = [UIAlertAction
+                                   actionWithTitle:NSLocalizedString(@"Clear", @"Cancel action")
+                                   style:UIAlertActionStyleCancel
+                                   handler:^(UIAlertAction *action)
+                                   {
+                                       textField.text = @"";
+                                   }];
+    
+    UIAlertAction *okAction = [UIAlertAction
+                               actionWithTitle:NSLocalizedString(@"OK", @"OK action")
+                               style:UIAlertActionStyleDefault
+                               handler:^(UIAlertAction *action)
+                               {
+                                   
+                               }];
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:okAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
